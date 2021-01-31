1267276bddaef2fb335ce83ad3c80febdaa6435b
David Hanson <davidhanson.c@gmail.com>
2021-01-31 13:42:24 -0700

    initial recursion exercise

--- algorithms/recursion/recursion_start.py
+++ algorithms/recursion/recursion_start.py
@@ -2,4 +2,15 @@
 # @Author: David Hanson
 # @Date:   2021-01-30 23:30:31
 # @Last Modified by:   David Hanson
-# @Last Modified time: 2021-01-30 23:30:31
+# @Last Modified time: 2021-01-31 13:41:39
+counter = 0
+
+def inception():
+    global counter
+    print(counter)
+    if counter > 3:
+        return "done!"
+    counter += 1
+    return inception()
+
+inception()
