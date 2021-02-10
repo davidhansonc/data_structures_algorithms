2aecc8070190f903017f10b53392fcfd213e5694
David Hanson <davidhanson.c@gmail.com>
2021-02-10 00:17:46 -0700

    first pass at successful implementation

---
+++ algorithms/sorting/merge_sort_implementation.py
@@ -0,0 +1,33 @@
+
+def merge_sort(number_list):
+    full_length = len(number_list)
+    if full_length > 1:
+        div_length = int(full_length / 2)
+        first_half = merge_sort(number_list[:div_length])
+        second_half = merge_sort(number_list[div_length:])
+        if type(first_half) is not int:
+            sorted_list = []
+            index_i, index_j = 0, 0
+            while index_i < len(first_half) and index_j < len(second_half):
+                first_num = first_half[index_i]
+                second_num = second_half[index_j]
+                if first_num < second_num:
+                    index_i += 1
+                    sorted_list.append(first_num)
+                else:
+                    index_j += 1
+                    sorted_list.append(second_num)
+            sorted_list.extend(min(first_half[index_i:], second_half[index_j:]))
+            sorted_list.extend(max(first_half[index_i:], second_half[index_j:]))
+            return sorted_list
+        else:
+            return [min(first_half, second_half), max(first_half, second_half)]
+    elif full_length == 1:
+        single_item = number_list[0]
+        return single_item
+
+
+numbers = [6, 3, 4, 1, 8, 7, 2, 5]
+# numbers = [6, 3, 4, 1]
+# numbers = [8, 7, 2, 5]
+print(merge_sort(numbers))
