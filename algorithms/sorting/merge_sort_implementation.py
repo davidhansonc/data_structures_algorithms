17009ddad2a13ae4643016d47cc2b8f917f206dd
David Hanson <davidhanson.c@gmail.com>
2021-02-10 15:49:13 -0700

    small merge sort update

--- algorithms/sorting/merge_sort_implementation.py
+++ algorithms/sorting/merge_sort_implementation.py
@@ -5,26 +5,30 @@ def merge_sort(number_list):
         div_length = int(full_length / 2)
         first_half = merge_sort(number_list[:div_length])
         second_half = merge_sort(number_list[div_length:])
-        if type(first_half) is not int:
-            sorted_list = []
-            index_i, index_j = 0, 0
-            while index_i < len(first_half) and index_j < len(second_half):
-                first_num = first_half[index_i]
-                second_num = second_half[index_j]
-                if first_num < second_num:
-                    index_i += 1
-                    sorted_list.append(first_num)
-                else:
-                    index_j += 1
-                    sorted_list.append(second_num)
-            sorted_list.extend(min(first_half[index_i:], second_half[index_j:]))
-            sorted_list.extend(max(first_half[index_i:], second_half[index_j:]))
-            return sorted_list
-        else:
-            return [min(first_half, second_half), max(first_half, second_half)]
+        return sort_return(first_half, second_half)
     elif full_length == 1:
         single_item = number_list[0]
         return single_item
+    
+
+def sort_return(first_half, second_half):
+    if type(first_half) is not int:
+        sorted_list = []
+        index_i, index_j = 0, 0
+        while index_i < len(first_half) and index_j < len(second_half):
+            first_num = first_half[index_i]
+            second_num = second_half[index_j]
+            if first_num < second_num:
+                index_i += 1
+                sorted_list.append(first_num)
+            else:
+                index_j += 1
+                sorted_list.append(second_num)
+        sorted_list.extend(min(first_half[index_i:], second_half[index_j:]))
+        sorted_list.extend(max(first_half[index_i:], second_half[index_j:]))
+        return sorted_list
+    else:
+        return [min(first_half, second_half), max(first_half, second_half)]
 
 
 numbers = [6, 3, 4, 1, 8, 7, 2, 5]
